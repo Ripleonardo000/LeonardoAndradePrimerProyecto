95b8d0b0cd93a41b5648055e265179a53227fd95
Leonardo <Leonardo@RIPLEO000>
2024-10-02 08:28:25 -0500

    Agregar archivos de proyecto.

---
+++ LeonardoAndradePrimerProyecto/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LeonardoAndradePrimerProyecto.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
